0f61581aaa7b2c434a1975354a4e6a2596a12dc5
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-07-02 21:15:20 +0700

    the code, calling the car object 5 times to accelerate, has been written

--- TestCar.py
+++ TestCar.py
@@ -1,8 +1,10 @@
 from CarClass import Car
 
-car_1 = Car(1997, "Mustang")
+car_1 = Car(1997, "make a car")
 
-car_1.brake()
-car_1.accelerate()
-current_speed = car_1.get_speed()
-print(car_1.get_speed())
+#calling the accelerate method 5 times. Assume that the unit of the speed of the car is meters per second (m/s)
+for i in range (5):
+    car_1.accelerate()
+    print("The current speed of the car is:", car_1.get_speed(), "m/s")
+
+
