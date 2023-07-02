37e0ca20b86c0fae0e12d62d063bd2c01d12c0df
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-07-02 16:24:52 +0700

    the code in order for the properties of the Fan#2 to be displayed has been written

--- TestFan.py
+++ TestFan.py
@@ -7,4 +7,10 @@ fan_2 = Fan(Fan.medium, 5, "blue", True)
 print("SPEED: ", fan_1.get_speed(), "[fast]")   #speed
 print("RADIUS:", fan_1.get_radius())            #radius
 print("COLOR: ", fan_1.get_color())             #color
-fan_1.set_on(False)
+fan_1.set_on(False)                             #on
+
+#for fan number 2
+print("\nSPEED: ", fan_2.get_speed(), "[medium]") #speed
+print("RADIUS:", fan_2.get_radius())            #radius
+print("COLOR: ", fan_2.get_color())             #color
+fan_2.set_on(True)                              #on
