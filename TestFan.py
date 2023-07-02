e85c630c4ec705cbcd5903b67b5ce27276b6222b
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-07-02 16:33:06 +0700

    ...

--- TestFan.py
+++ TestFan.py
@@ -6,16 +6,16 @@ fan_2 = Fan(Fan.medium, 5, "blue", True)
 
 #for Fan number 1
 print(Fore.YELLOW + Style.NORMAL + "FAN #1:", Fore.WHITE + Style.NORMAL + "=" * 86)
-print(Fore.YELLOW + Style.DIM + "SPEED: ", Fore.WHITE + Style.NORMAL + str(fan_1.get_speed()), "[fast]")
-print(Fore.YELLOW + Style.DIM + "RADIUS:", Fore.WHITE + Style.NORMAL + str(fan_1.get_radius()))
-print(Fore.YELLOW + Style.DIM + "COLOR: ", Fore.WHITE + Style.NORMAL + str(fan_1.get_color()))
-fan_1.set_on(False)
+print(Fore.YELLOW + Style.DIM + "SPEED: ", Fore.WHITE + Style.NORMAL + str(fan_1.get_speed()), "[fast]")    #speed
+print(Fore.YELLOW + Style.DIM + "RADIUS:", Fore.WHITE + Style.NORMAL + str(fan_1.get_radius()))             #radius
+print(Fore.YELLOW + Style.DIM + "COLOR: ", Fore.WHITE + Style.NORMAL + str(fan_1.get_color()))              #color
+fan_1.set_on(False)     #on
 print(Fore.WHITE + Style.NORMAL + "=" * 94)
 
 #for Fan number 2
 print(Fore.CYAN + Style.NORMAL + "\nFAN #2:", Fore.WHITE + Style.NORMAL + "=" * 86)
-print(Fore.CYAN + Style.DIM + "SPEED: ", Fore.WHITE + Style.NORMAL + str(fan_2.get_speed()), "[medium]")
-print(Fore.CYAN + Style.DIM + "RADIUS:", Fore.WHITE + Style.NORMAL + str(fan_2.get_radius()))
-print(Fore.CYAN + Style.DIM + "COLOR: ", Fore.WHITE + Style.NORMAL + str(fan_2.get_color()))
-fan_2.set_on(False)
+print(Fore.CYAN + Style.DIM + "SPEED: ", Fore.WHITE + Style.NORMAL + str(fan_2.get_speed()), "[medium]")    #speed
+print(Fore.CYAN + Style.DIM + "RADIUS:", Fore.WHITE + Style.NORMAL + str(fan_2.get_radius()))               #radius
+print(Fore.CYAN + Style.DIM + "COLOR: ", Fore.WHITE + Style.NORMAL + str(fan_2.get_color()))                #color
+fan_2.set_on(True)     #on
 print(Fore.WHITE + Style.NORMAL + "=" * 94)
