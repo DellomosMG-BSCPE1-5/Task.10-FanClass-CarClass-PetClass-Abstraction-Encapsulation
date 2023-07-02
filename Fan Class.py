934ddc45db4571317536fe3a39ed73d83ee77ffd
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-07-02 10:35:10 +0700

    Constructor has been created

--- Fan Class.py
+++ Fan Class.py
@@ -5,4 +5,9 @@ class Fan:
     medium = 2
     fast = 3
 #create a parameterized constructor
+    def __init__(self, speed = slow, radius = 5, color = "blue", on = False):
+        self.__speed = speed
+        self.__radius = radius
+        self.__color = color
+        self.__on = on
 #create the methods
