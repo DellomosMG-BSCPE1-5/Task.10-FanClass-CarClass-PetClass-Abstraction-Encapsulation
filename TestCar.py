a71697decc0f5e2417f1b033a60bb136b8441629
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-07-02 21:22:35 +0700

    the code, calling the car object 5 times to deccelerate, has been written

--- TestCar.py
+++ TestCar.py
@@ -7,4 +7,8 @@ for i in range (5):
     car_1.accelerate()
     print("The current speed of the car is:", car_1.get_speed(), "m/s")
 
+#calling the accelerate method 5 times. Assume that the unit of the speed of the car is meters per second (m/s)
+for i in range (5):
+    car_1.brake()
+    print("The current speed of the car is:", car_1.get_speed(), "m/s")
 
