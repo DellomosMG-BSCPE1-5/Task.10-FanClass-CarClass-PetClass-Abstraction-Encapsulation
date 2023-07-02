c780d4358490c0ec76b0bc8579a4f4ac7739cd55
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-07-02 18:51:59 +0700

    the accelerate, brake, and get_speed methods have been created

--- CarClass.py
+++ CarClass.py
@@ -5,10 +5,21 @@ class Car:
         self.__year_model = year_model
         self.__make = make
         self.__speed = speed
+
 #create the necessary methods
 #accelerate()
+    def accelerate(self):
     #the speed data attribute will increase by 5 
+        self.__speed += 5
 #brake()
-    #the speed data attribute will decrease by 5 
+    def brake(self):
+    #if the  speed data attribute is equal or less than to zero, then it will be equal to zero.
+        if self.__speed <= 0:
+            self.__speed = 0
+    #else, the speed data attribute will decrease by 5
+        else:
+            self.__speed -= 5
 #get_speed()
+    def get_speed(self):
     #return the current speed
+        return self.__speed
--- TestCar.py
+++ TestCar.py
@@ -1,1 +1,7 @@
 from CarClass import Car
+
+car_1 = Car(1997, "Mustang", 0)
+
+car_1.accelerate()
+current_speed = car_1.get_speed()
+print(car_1.get_speed())
