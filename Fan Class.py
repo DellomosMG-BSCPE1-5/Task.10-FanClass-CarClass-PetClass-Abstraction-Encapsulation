740d25389e5caeca7710e49a83f6f7c823473e28
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-07-02 10:26:38 +0700

    Class Fan has been created and constants are declared.

--- Fan Class.py
+++ Fan Class.py
@@ -1,4 +1,8 @@
 #create a class named "Fan"
+class Fan:
 #declare constants to denote the fan speed
+    slow = 1
+    medium = 2
+    fast = 3
 #create a parameterized constructor
 #create the methods
