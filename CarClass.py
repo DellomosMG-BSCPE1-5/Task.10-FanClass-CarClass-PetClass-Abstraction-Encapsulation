1594b6956b316032d0a59cb6eac9160c4cda58ac
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-07-02 18:03:02 +0700

    the CarClass file and the TestCar file have been created. The pseudocode as well has been written.

---
+++ CarClass.py
@@ -0,0 +1,9 @@
+#create a class named Car
+#create the constructor
+#create the necessary methods
+#accelerate()
+    #the speed data attribute will increase by 5 
+#brake()
+    #the speed data attribute will decrease by 5 
+#get_speed()
+    #return the current speed
