3e43f92e72748ec373ad64daf34cc083002e87e2
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-07-03 18:02:52 +0700

    Left and Right Frames, done

--- PetProgram_GUI.py
+++ PetProgram_GUI.py
@@ -13,6 +13,14 @@ window.configure(bg="#0F1524")
 #headers
 heading = Label(text="Your Pet", fg="#8FBB3F", bg="#0F1524", width="450",height="1", font=("Helvetica", 20, "bold"))
 heading.pack(fill=X)
+
+#Frames
+left_frame=Frame(window,bg="#086387",relief=RIDGE,bd=10)
+left_frame.place(x=10,y=50,width=345,height=250)
+right_frame=Frame(window,bg="#086387",relief=RIDGE,bd=10)
+right_frame.place(x=365,y=50,width=345,height=250)
+
+#Labels
 #create the buttons
 #create the necessary functions of the buttons
 
