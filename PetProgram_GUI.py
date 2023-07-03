277400c79445be6beb30819242a3012d3cb3feb9
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-07-03 17:33:38 +0700

    GUI window has been created

--- PetProgram_GUI.py
+++ PetProgram_GUI.py
@@ -3,8 +3,14 @@ from tkinter import *
 from tkinter import ttk,messagebox
 import os
 
-#declare the variables
 #create the window
+root=Tk()
+root.title('Your Pet')
+root.geometry('720x360')
+
+#declare the variables
 #design/create the frames
 #create the buttons
 #create the necessary functions of the buttons
+
+root.mainloop()
