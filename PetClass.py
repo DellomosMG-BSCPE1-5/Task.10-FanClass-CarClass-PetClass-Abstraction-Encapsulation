5ac0ea001d404403068f3d920dba6fb73d861a42
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-07-03 11:45:08 +0700

    the accessor and mutator methods for all of the data attributes have been written

--- PetClass.py
+++ PetClass.py
@@ -1,13 +1,35 @@
 #create class named Pet
 class Pet:
     #constructor
-    def __init__(self, name, animal_type, age):
+    def __init__(self, name = " ", animal_type = " ", age = 0):
         #attributes
         self.__name = name
         self.__animal_type = animal_type
         self.__age = age
-#methods
-#accessor methods for all data attributes
-#mutator for all data attributes
 
+    #methods
+    #mutator for all data attributes
+    def set_name(self):
+        self.__name = str(input("Enter the name of your pet: "))
+    
+    def set_animal_type(self):
+        self.__animal_type = str(input("Enter your pet's animal type: "))
+    
+    def set_age(self):
+        while True:
+            try:
+                self.__age = int(input("Enter the age of you pet: "))
+            except (ValueError):
+                print(" Invalid Input. Please enter whole number only.")
+            else:
+                break   
 
+    #accessor methods for all data attributes
+    def get_name(self):
+        return self.__name
+    
+    def get_animal_type(self):
+        return self.__animal_type
+    
+    def get_age(self):
+        return self.__age
--- TestPet.py
+++ TestPet.py
@@ -1,5 +1,17 @@
 from Headers import Header
-
+from PetClass import Pet
 #header
 pet_header = Header()
 pet_header.pet()
+
+pet_1 = Pet()
+pet_1.set_age()
+
+
+
+
+
+
+#display
+
+print("age: ", pet_1.get_age())
