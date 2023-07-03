d9047dd5f4b4e2a6caaf13bc27f603d0f0eaf6fe
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-07-03 11:10:22 +0700

    class pet and constructor, done

--- PetClass.py
+++ PetClass.py
@@ -1,7 +1,13 @@
 #create class named Pet
-#constructor
-#attributes
+class Pet:
+    #constructor
+    def __init__(self, name, animal_type, age):
+        #attributes
+        self.__name = name
+        self.__animal_type = animal_type
+        self.__age = age
 #methods
 #accessor methods for all data attributes
 #mutator for all data attributes
+
 
