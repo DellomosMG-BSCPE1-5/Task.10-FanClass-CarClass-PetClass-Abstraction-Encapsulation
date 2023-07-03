cf2180585d7c72462a3367d7fdb94dd8f0e4888d
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-07-03 15:46:55 +0700

    the code that displays pet's data has been written

--- TestPet.py
+++ TestPet.py
@@ -1,3 +1,4 @@
+from colorama import Fore, Style
 from Headers import Header
 from PetClass import Pet
 
@@ -14,4 +15,16 @@ while True:
     pet_data.set_name()
     pet_data.set_animal_type()
     pet_data.set_age()
+
+    #displaying pet's data
+    print(Fore.YELLOW + Style.NORMAL + "\nYOUR PET'S DATA:", Fore.WHITE + Style.NORMAL + "=" * 69)
+    print(Fore.YELLOW + Style.DIM + "NAME:       ", Fore.WHITE + Style.NORMAL + str(pet_data.get_name()))        #name
+    print(Fore.YELLOW+ Style.DIM + "ANIMAL TYPE:", Fore.WHITE + Style.NORMAL + str(pet_data.get_animal_type()))  #animal type
+    print(Fore.YELLOW + Style.DIM + "AGE:        ", Fore.WHITE + Style.NORMAL + str(pet_data.get_age()), "years old")         #age
+    print(Fore.WHITE + Style.NORMAL + "=" * 86)
+
+    again = input("\nDo you want to input data again? Type Y to restart or press another key to quit: ")  # Asking the user if he/she wants to try/start again.
+    #if user typed "y", go back to the top.
+    if again.upper() == "Y":  
+        continue
 
