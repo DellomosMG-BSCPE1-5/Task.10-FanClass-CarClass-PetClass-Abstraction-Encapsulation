9e9b5c9df84e723c418245cb24ef4bc51e5c07ce
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-07-03 18:23:06 +0700

    Labels, done

--- PetProgram_GUI.py
+++ PetProgram_GUI.py
@@ -21,6 +21,16 @@ right_frame=Frame(window,bg="#086387",relief=RIDGE,bd=10)
 right_frame.place(x=365,y=50,width=345,height=250)
 
 #Labels
+name = Label(left_frame,text="Your Pet's Name",font=('kacstdigital',15,'bold'), fg='#FCB912',bg="#086387")
+name.grid(row=5,column=0,padx=10,pady=20)
+
+animal_type = Label(left_frame,text='Animal Type',font=('kacstdigital',15,'bold'),fg ='#FCB912',bg="#086387")
+animal_type.grid(row=6,column=0,padx=10,pady=20)
+
+pet_age = Label(left_frame,text="Your Pet's Age",font=('kacstdigital',15,'bold'),fg ='#FCB912',bg="#086387")
+pet_age.grid(row=7,column=0,padx=10,pady=20)
+
+
 #create the buttons
 #create the necessary functions of the buttons
 
